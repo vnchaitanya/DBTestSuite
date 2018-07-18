a6ce3bdf0ef32eec62dc9bc88a600d0676f2b025
SwathiReddy-p <quadrantlenora@outlook.com>
2018-07-17 23:55:56 -0700

    Created Functions CheckExitStatus ,LogMsg and commands to install Influxdb

--- LinuxScripts/SampleScript.sh
+++ LinuxScripts/SampleScript.sh
@@ -1,1 +1,12 @@
-apt-get update
+#Commands to Install Influxdb
+sudo apt-get update
+CheckExitStatus "ExitOnError"
+LogMsg "Info:Updating pacakges"
+
+sudo apt-get install influxdb
+CheckExitStatus "ExitOnError"
+LogMsg "Info:Installing latest version of Influxdb"
+
+sudo apt install influxdb-client
+CheckExitStatus "ExitOnError"
+LogMsg "Info:Installing influxdb client"
