b8047140795f70dc003059960cf2bd349e483fe2
Srikanth Myakam <m.srikanth.m@gmail.com>
2018-07-19 12:13:43 -0700

    Improved LogMsg

--- LinuxScripts/LinuxLib.sh
+++ LinuxScripts/LinuxLib.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
+
 # checking the status whether it is success or not
-function CheckExitStatus()
+function CheckExitStatus
 {
-    if [ "$?" -ne "0" ];
+    if [ $? -ne "0" ]
     then
-        echo "FAILURE";
+        echo "FAILURE"
             #If the installation fails it will exit the code
-            if [ "$1"  -eq  "ExitOnError" ];
+            if [ $1  -eq  "ExitOnError" ]
             then
-                exit 1;
+                exit 1
             fi
     else
-        echo "SUCCESS";
+        echo "SUCCESS"
     fi
 }
 
 #Adding a time stamp and write the message to test log
-function LogMsg()
+function LogMsg
 {
-  echo $(date)  "$1"
+    now=`date`
+    echo $now "$1"
+    echo $now "$1" >> $LogFile
 }
-
+##########################################################
+#
+##########################################################
+#defining a log file
+LogFile="ExecutionLog.log"
+#Pushing old logs to 'ExecutionLog.log.old'
+if [ -f $LogFile ]
+then
+    cat $LogFile >> $LogFile.old
+fi
