998b9e094c0bf974433154a5bcf8001d190f784e
SwathiReddy-p <quadrantlenora@outlook.com>
2018-07-18 21:10:54 -0700

    Kept Installing Influxdb code in InstallInfluxRepo.sh

--- LinuxScripts/InstallInfluxRepo.sh
+++ LinuxScripts/InstallInfluxRepo.sh
@@ -1,2 +1,15 @@
 . ./LinuxLib.sh
-. ./SampleScript.sh
+
+#Commands to Install Influxdb
+sudo apt-get update
+CheckExitStatus "ExitOnError"
+LogMsg "Info:Updating pacakges"
+
+sudo apt-get install influxdb
+CheckExitStatus "ExitOnError"
+LogMsg "Info:Installing latest version of Influxdb"
+
+sudo apt install influxdb-client
+CheckExitStatus "ExitOnError"
+LogMsg "Info:Installing influxdb client"
+
